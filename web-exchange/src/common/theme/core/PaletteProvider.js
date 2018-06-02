1a9c26864dd5ade97fabe8e89d7c0606bfec5b75
Zack Rubenstein <zrubenst@gmail.com>
2018-06-01 23:20:03 -0400

    Resolved requested changes in PR

--- web-exchange/src/common/theme/core/PaletteProvider.js
+++ web-exchange/src/common/theme/core/PaletteProvider.js
@@ -1,20 +1,15 @@
-import ColorPalette from '../color-palette.scss';
-
 /*
- * color-palette.scss calls (@includes) `export-color-palette()` (defined 
- * in core/palette-helper.scss) which outputs the JSON representation
- * of the color palette into the `content` attribute of body::before.
- * This function extracts the string json content of body::before and
- * parses it to JSON.
+ * The SCSS color palette hash is stored in the `content` of 
+ * body::before as JSON. This reads and parses the color palette
  */
-function loadPaletteFromSCSS() {
+function readPaletteFromSCSS() {
   try {
-    const content = window.getComputedStyle(
-      document.querySelector('body'), ':before'
-    ).getPropertyValue('content')
-      .replace(/(^"|"$)/g, '') // remove enclosing double quotes
+    const content = window
+      .getComputedStyle(document.querySelector("body"), ":before")
+      .getPropertyValue("content")
+      .replace(/(^"|"$)/g, "") // remove enclosing double quotes
       .replace(/\\"/g, '"'); // remove all escaped quotes
-    
+
     return JSON.parse(content);
   } catch (e) {
     return {};
@@ -23,11 +18,5 @@ function loadPaletteFromSCSS() {
 
 /*
  * Javascipt object representation of the scss map in color-palette.scss
- *
- * Usage: import { COLOR_PALETTE } from './path/to/.../PaletteProvider';
- *
- * NOTE: this should only be used internally by DolomiteTheme.js 
- * and OverrideHelper.js. Read the Theme ReadMe to learn how to use
- * colors in this project
  */
-export const COLOR_PALETTE = loadPaletteFromSCSS();
+export const COLOR_PALETTE = readPaletteFromSCSS();
