2eebebd252cf79a47b313ebddec05ea9c2c4882c
Zack Rubenstein <zrubenst@gmail.com>
2018-06-05 19:23:28 -0400

    Cleaned up code. Using better HOCs for using/changing the Theme/Colors, and used more functional programming

--- web-exchange/src/components/Navbar/Navbar.js
+++ web-exchange/src/components/Navbar/Navbar.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import Button from "@material-ui/core/Button";
 
-import styles from './Navbar.scss';
-import { dolomiteThemeDispatcher } from '../../common/theme/DolomiteTheme';
-
-const SwapThemeButton = dolomiteThemeDispatcher(changeTheme => (
-  <Button className={styles.changetheme} onClick={() => changeTheme()}>
-    Swap Theme
-  </Button>
-));
+import styles from "./Navbar.scss";
+import { withThemeChanger } from "../../common/theme/DolomiteTheme";
 
 export const Navbar = props => (
   <nav>
     <h3 className={styles.title}>Navbar: {props.userName}</h3>
 
-    {SwapThemeButton}
+    <Button
+      className={styles.changetheme}
+      onClick={() => props.requestThemeChange()}
+    >
+      Swap Theme
+    </Button>
   </nav>
 );
 
@@ -25,11 +24,11 @@ const mapStateToProps = state => ({
 });
 
 Navbar.defaultProps = {
-  userName: 'Default Name Navbar'
+  userName: "Default Name Navbar"
 };
 
 Navbar.propTypes = {
   userName: PropTypes.string
 };
 
-export default connect(mapStateToProps)(Navbar);
+export default connect(mapStateToProps)(withThemeChanger(Navbar));
